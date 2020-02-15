6758ad07b9803f2f1b255f23e5ddab9a69195772
GMC2020 <ahmedbalti06@gmail.com>
2020-02-15 16:20:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+export default class App extends React.Component{
+
+  componentWillMount(){
+    setTimeout(function(){ alert("Hello"); }, 3000);
+  }
+  componentWillUnmount(){
+
+    window.onbeforeunload=function(){
+      alert ("bye")
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    )
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,3 +58,4 @@ function App() {
 }
 
 export default App;
+ */
